5500435aa996452a7a161e2ed046409849b42dba
Boris <b.mulder@appec.nl>
2019-09-27 18:31:11 +0200

    Update Database.java
    able to insert and update items in the database

--- src/Db/Database.java
+++ src/Db/Database.java
@@ -1,11 +1,12 @@
 package Db;
 
 import Layouts.App;
-
 import java.sql.*;
 
 import static Layouts.App.AppException;
 import static Layouts.App.Info;
+import static Layouts.App.Error;
+import static Layouts.App.Warning;
 import static Utils.Enums.DbVersions;
 
 /**
@@ -19,96 +20,122 @@ public class Database {
     private static final String LiveDbConnection = "";
     private static final StringBuilder QueryBuilder = new StringBuilder();
 
-    private static final  String USER_TABLE = "citibank_user_accounts";
+    private static final String USER_TABLE = "citibank_user_accounts";
+    private static final String BANK_ACCOUNTS_TABLE = "citibank_bank_accounts";
     private static final String[] USER_TABLE_PARAMS = {
             "userName","surName","userdBirth","userPhone","userEmail","userregisterDate","userBankAccountId"
     };
 
     private static String ConnectionString;
-
     private static String UserName = "";
     private static String Password = "";
 
 
-    public static boolean CreateUserAccount(Object[] values)
+    public static boolean CreateUserAccount(Object[] values,double balance)
     {
-        return InsertQuery(USER_TABLE,USER_TABLE_PARAMS,values);
+        DatabaseResult userAccount = InsertQuery(USER_TABLE,USER_TABLE_PARAMS,values);
+
+        if(userAccount.isSucces())
+        {
+            DatabaseResult userBankAccount = CreateUserBankAccount(userAccount.getLastInsertedId(),balance);
+
+            userAccount = UpdateQuery(USER_TABLE,new String[]{"userBankAccountId"},new Object[]{userBankAccount.getLastInsertedId()},"userId",userBankAccount.getLastInsertedId());
+
+            return userBankAccount.isSucces() && userAccount.isSucces();
+        }
+
+        return false;
     }
 
-    public static void CreateUserBankAccount(String[] values)
+    private static DatabaseResult CreateUserBankAccount(int accountId,double balance)
     {
-
-        // TODO
-        if (InsertQuery("",new String[]{"userName",""},values))
-        {
-
-        }
-        else
-        {
-
-        }
+        return InsertQuery(BANK_ACCOUNTS_TABLE,new String[]{"balance","accountId"},new Object[]{balance,accountId});
     }
 
-
-    private static boolean InsertQuery(String table, String[] params, Object[] values)
+    private static DatabaseResult UpdateQuery(String table, String[] params, Object[] values, String condition, Object conditionValue)
     {
         try {
-            QueryBuilder.delete(0,QueryBuilder.length());
+            Connection connection = GetConnection();
 
-            Class.forName(JDBCDriver);
-
-            Connection connection = DriverManager.getConnection(ConnectionString,UserName,Password);
-
-            StringBuilder queryBuilder = new StringBuilder();
-
-            queryBuilder.append(String.format("INSERT INTO %s (",table));
+            QueryBuilder.append(String.format("UPDATE %s SET ",table));
 
             for(int index = 0; index < params.length; index++)
             {
-                if(index == params.length-1)
-                {
-                    queryBuilder.append(String.format("%s)",params[index]));
-                }
-                else
-                    queryBuilder.append(String.format("%s,",params[index]));
+                QueryBuilder.append(String.format("%s = ?%s",params[index],(index == params.length-1 ? "" : ",")));
             }
 
-            queryBuilder.append(" VALUES (");
+            QueryBuilder.append(String.format(" WHERE %s = %s",condition,conditionValue.toString()));
+            PreparedStatement stmt = connection.prepareStatement(QueryBuilder.toString(),Statement.RETURN_GENERATED_KEYS);
 
-            for(int index = 0; index < params.length; index++)
+            FillQuery(values, stmt);
+
+            int id = stmt.executeUpdate();
+            ResultSet set = stmt.getGeneratedKeys();
+
+            if(set.next())
             {
-                System.out.println("V:"+index);
-                if(index == params.length-1)
-                {
-                    queryBuilder.append("?);");
-                }
-                else
-                    queryBuilder.append("?,");
+                return new DatabaseResult(set.getInt(1), true);
             }
 
-            PreparedStatement stmt = connection.prepareStatement(queryBuilder.toString());
-
-            for(int index = 0; index < values.length; index++)
-            {
-                System.out.println("P:"+index);
-                if(values[index] instanceof Integer)
-                {
-                    stmt.setInt(index+1,(int)values[index]);
-                }
-
-                if(values[index] instanceof String)
-                {
-                    stmt.setString(index+1,(String)values[index]);
-                }
-            }
-
-            stmt.executeUpdate();
-            return true;
+            Warning("Failed to read generated keys, contact the developer!");
+            return new DatabaseResult(-1,true);
         }
         catch (Exception e)
         {
-            e.printStackTrace();
-            return false;
+            App.Error(e.getMessage());
+            return new DatabaseResult(-1,false);
+        }
+    }
+
+    private static DatabaseResult InsertQuery(String table, String[] params, Object[] values)
+    {
+        try {
+            Connection connection = GetConnection();
+
+            QueryBuilder.append(String.format("INSERT INTO %s (",table));
+
+            for(int index = 0; index < params.length; index++)
+            {
+                if(index == params.length-1)
+                {
+                    QueryBuilder.append(String.format("%s)",params[index]));
+                }
+                else
+                    QueryBuilder.append(String.format("%s,",params[index]));
+            }
+
+            QueryBuilder.append(" VALUES (");
+
+            for(int index = 0; index < params.length; index++)
+            {
+                if(index == params.length-1)
+                {
+                    QueryBuilder.append("?);");
+                }
+                else
+                    QueryBuilder.append("?,");
+            }
+
+            PreparedStatement stmt = connection.prepareStatement(QueryBuilder.toString(),Statement.RETURN_GENERATED_KEYS);
+
+            FillQuery(values,stmt);
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            if(rs.next())
+            {
+                return new DatabaseResult(rs.getInt(1),true);
+            }
+
+            Warning("Failed to read generated keys, contact the developer!");
+            return new DatabaseResult(-1,true);
+        }
+        catch (Exception e)
+        {
+            Error(e.getMessage());
+            return new DatabaseResult(-1,false);
         }
     }
 
@@ -131,6 +158,34 @@ public class Database {
                 break;
 
             default: AppException("Failed to set Db connection");
+        }
+    }
+
+    private static Connection GetConnection() throws Exception
+    {
+        QueryBuilder.setLength(0);
+        Class.forName(JDBCDriver);
+        return DriverManager.getConnection(ConnectionString,UserName,Password);
+    }
+
+    private static void FillQuery(Object[] values,PreparedStatement stmt) throws Exception
+    {
+        for(int index = 0; index < values.length; index++)
+        {
+            if(values[index] instanceof Integer)
+            {
+                stmt.setInt(index+1,(int)values[index]);
+            }
+
+            if(values[index] instanceof String)
+            {
+                stmt.setString(index+1,values[index].toString());
+            }
+
+            if(values[index] instanceof Double)
+            {
+                stmt.setDouble(index+1,(double)values[index]);
+            }
         }
     }
 }
