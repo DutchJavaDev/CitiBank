5ec8a34fad51f4523256932b47c0b7c6e8df26b4
Boris <b.mulder@appec.nl>
2019-09-27 18:32:37 +0200

    Update Main.java
    removed unused code

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import Layouts.App;
 
-import java.sql.*;
-
 public class Main extends Application {
 
     @Override
@@ -22,29 +20,6 @@ public class Main extends Application {
 
         primaryStage.show();
 }
-
-
-    public void test()
-    {
-        String connectionString = "jdbc:mysql://localhost/citibankDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection(   connectionString,"root","");
-            //here sonoo is database name, root is username and password
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from dump");
-            while(rs.next())
-            {
-                System.out.println(rs.getInt(rs.findColumn("id"))+"  "+rs.getString(rs.findColumn("account"))+"  ");
-            }
-
-            con.close();
-            System.out.println("Done");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-    }
-
 
     public static void main(String[] args) {
         launch(args);
